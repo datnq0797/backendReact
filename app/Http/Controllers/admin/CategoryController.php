98a56fe2ff9f65fdd5aafd763a044a8433820825
datnq0797 <62018689+datnq0797@users.noreply.github.com>
2026-07-15 14:54:28 +0700

    brand cate controller

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::orderBy('created_at', 'DESC')->get();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $categories,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $category = new Category;
+        $category->name = $request->name;
+        $category->status = $request->status;
+        $category->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category created successfully',
+            'data' => $category,
+        ], 200); // 200 HTTP Status Code
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if (! $category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => [],
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $category,
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        if (! $category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => [],
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $category->name = $request->name;
+        $category->status = $request->status;
+        $category->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category updated successfully',
+            'data' => $category,
+        ], 200); // 200 HTTP Status Code
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        if (! $category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found',
+                'data' => [],
+            ], 404);
+        }
+
+        $category->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category deleted successfully',
+            'data' => [],
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AuthController;
+use App\Http\Controllers\admin\CategoryController;
+
 Route::post('/admin/login', [AuthController::class, 'authenticate']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    // Route::get('categories', [CategoryController::class, 'index']);
+    // Route::get('categories/{id}', [CategoryController::class, 'show']);
+    // Route::post('categories', [CategoryController::class, 'store']);
+    // Route::put('categories/{id}', [CategoryController::class, 'update']);
+    // Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('brands', CategoryController::class);
+});
